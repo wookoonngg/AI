f3ba370e9ec856cde8611f6eb632c22bbd46ab75
woobin <guiyomyee@gmail.com>
2025-02-25 17:27:16 +0900

    v1.5

--- Day12.py
+++ Day12.py
@@ -4,17 +4,63 @@
 import numpy as np
 import pandas as pd
 from sklearn.impute import SimpleImputer
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.model_selection import train_test_split
 
-df = pd.DataFrame(
-    {
-        'A' : [1,2, np.nan,4],
-        'B' : [np.nan, 12,3,4],
-        'C' : [1,2,3,4]
-    }
-)
 
-print(df)
+# 나이에 따른 생존율 계산
 
-df.fillna(df.mean(), inplace = True)
+titanic = sns.load_dataset('titanic')  # 데이터 로딩
+median_age = titanic['age'].median()  # 나이 중앙값 산출
+titanic_fill_row = titanic.fillna({'age' : median_age})  # 결측치 처리
 
-print ("평균으로 채운 뒤 데이터\n" , df)
+X = titanic_fill_row[['age']]  # 독립 변수 설정
+y = titanic_fill_row[['survived']]  # 종속 변수 설정
+
+# 훈련/검증 데이터 분할
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# 모델 선택
+model = KNeighborsRegressor(n_neighbors=5)
+
+# K 최근접 이웃 회귀 모델 훈련
+model.fit(X_train, y_train)
+
+# 예측
+y_pred = model.predict(X_test)  # 검증 셋트를 인수로 예측
+
+# 시각화
+plt.figure(figsize=(5, 2))
+plt.scatter(X_test, y_test, color='blue', label='Real')
+plt.scatter(X_test, y_pred, color='red', label='Predicted')
+plt.title('KNeighborsRegressor: Real vs Predicted')
+plt.xlabel('Age')
+plt.ylabel('Survivied')
+plt.show()
+
+
+'''
+[seaborn 클래스 함수 구조]
+survived -> 1이면 생존 0 이면 사망
+Pclass -> 선실 등급 1~3등급까지
+Fare -> 티켓에 대해 지불한 금액
+'''
+
+
+
+
+# df = pd.DataFrame(
+#     {
+#         'A' : [1,2, np.nan,4],
+#         'B' : [np.nan, 12,3,4],
+#         'C' : [1,2,3,4]
+#     }
+# )
+#
+# print(df)
+#
+# df.fillna(df.mean(), inplace = True)
+#
+# print ("평균으로 채운 뒤 데이터\n" , df)
